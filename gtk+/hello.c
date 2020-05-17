2e32425b4b6e28a0b3ef3647b69e51ba36c8b40e
si0005hp <si0005hp@gmail.com>
2020-05-17 23:36:50 +0900

    gtk+

---
+++ gtk+/hello.c
@@ -0,0 +1,25 @@
+#include <gtk/gtk.h>
+
+#define LABEL_WIDTH 200
+#define LABEL_HEIGHT 50
+
+int main(int argc, char *argv[])
+{
+  GtkWidget *window;
+  GtkWidget *label;
+
+  gtk_init(&argc, &argv);
+
+  window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  g_signal_connect(G_OBJECT(window), "destroy",
+                   G_CALLBACK(gtk_main_quit), NULL);
+
+  label = gtk_label_new("Hello, world!");
+  gtk_widget_set_size_request(label, LABEL_WIDTH, LABEL_HEIGHT);
+  gtk_container_add(GTK_CONTAINER(window), label);
+  gtk_widget_show_all(window);
+
+  gtk_main();
+
+  return 0;
+}
